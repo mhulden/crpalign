61b5dd210d649a5a953f0ac0e68acc9724a1a0a9
mhulden <mans.hulden@gmail.com>
2022-04-01 12:35:14 -0600

    Update example.py

--- example.py
+++ example.py
@@ -1,7 +1,6 @@
 import align
 
 lines = [l.strip().split() for l in open("englishpron.txt")]
-pairs = [(spelling, pron) for pron, spelling in lines]
 
 ap = align.Aligner(lines, '_', iterations = 50, mode = 'crp')
 for input, output in ap.alignedpairs:
